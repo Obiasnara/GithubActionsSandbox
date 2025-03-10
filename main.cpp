7ffa2ea56bbc71a3ccfdccc5bac90c4fda82c888
Nicolas Picaud <nicolas.picaud@etu.univ-savoie.fr>
2025-03-10 15:19:50 +0100

    Linter Action Test

---
+++ main.cpp
@@ -0,0 +1,37 @@
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    if(1) 
+    { 
+        std::cout << "This is true" << std::endl;
+    }
+
+    if (0) {
+
+
+        std::cout << "This is false" << std::endl;
+
+
+    }
+
+    return 0;
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
